3a92d9e012f9c48fafb6c7d339323b5228b83934
EsraAlhaddad <israalhaddad9812@gmail.com>
2024-07-31 15:27:26 +0300

    first commit

---
+++ CikisKatmani.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YSA
+{
+    class CikisKatmani
+    {
+        public Noron[] cikisKatmanNoronlari;
+
+
+        public CikisKatmani(int cikisKatmanSayisi)
+        {
+            cikisKatmanNoronlari = new Noron[cikisKatmanSayisi];
+
+            for (int i = 0; i < cikisKatmanSayisi; i++)
+            {
+                Noron noron = new Noron();
+
+                // Çıkış katmanın değerleri 0'lanır
+                noron.cikisValue = 0;
+                //Oluşturulan nöron, cikisKatmanNoronlari dizisine eklenir.
+                cikisKatmanNoronlari[i] = noron;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NeuralNetwork neuralNetwork = new NeuralNetwork();
+
+        int[] butonGirisDegerleri = new int[35]
+        {
+          0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0
+        };
+
+        private void btnEgit_Click(object sender, EventArgs e)
+        {
+            Veriler veriler = new Veriler();
+            bool durum = neuralNetwork.Training(veriler.veriler, veriler.beklenenler);
+            if (durum)
+            {
+                btnEgit.Enabled = true;
+                button36.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int deger = int.Parse(btn.Name.Substring(6));
+            if(btn.BackColor == Color.White)
+            {
+                butonGirisDegerleri[deger - 1] = 1;
+                btn.BackColor = Color.Black;
+            }
+            else
+            {
+                butonGirisDegerleri[deger - 1] = 0;
+                btn.BackColor = Color.White;
+            }
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            neuralNetwork.tahmin(butonGirisDegerleri);
+            lblASonuc.Text = neuralNetwork.sonuc[0].ToString();
+            lblBSonuc.Text = neuralNetwork.sonuc[1].ToString();
+            lblCSonuc.Text = neuralNetwork.sonuc[2].ToString();
+            lblDSonuc.Text = neuralNetwork.sonuc[3].ToString();
+            lblESonuc.Text = neuralNetwork.sonuc[4].ToString();
+
+        }
+
+        private void temizle_Click(object sender, EventArgs e)
+        {
+            // Tüm kutuları beyaza çevir
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button && control.Name.StartsWith("button"))
+                {
+                    Button btn = (Button)control;
+                    btn.BackColor = Color.White;
+                }
+            }
+
+            // butonGirisDegerleri dizisini sıfırla
+            Array.Clear(butonGirisDegerleri, 0, butonGirisDegerleri.Length);
+        }
+
+    }
+}
+
